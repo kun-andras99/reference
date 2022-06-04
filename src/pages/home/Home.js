73309f070178294468553e8942799ea29c7c3915
kunandras-99 <kunandras99@gmail.com>
2022-06-04 15:13:44 +0200

    start

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,7 +10,7 @@ function Home() {
             <Heading text="1">Kezdőlap</Heading>
             <Text>Kedves látogató, üdvözöllek a NarutoRPG weboldalon!</Text>
             <Text>Ez a Naruto MMORPG Online játék a Naruto Anime sorozat egy fanoldala, ahol próbáljuk visszaadni az Anime nyújtotta élményt egy kattintós játékon keresztül, kisebb változásokkal és átalakításokkal. Törekszünk minél több infomációval és tartalommal ellátni weboldalunkat a jobb játékélményhez! Későbbiekben várjuk a látogatóinkat ingyenes regisztrációval!</Text>
-            <Text>Ahhoz hogy el tudd kezdeni a játékot először regisztrálnod kell, utána bejelentkezned, majd egy karaktert létrehoznod. Ne aggódj, ha csak egy kedvenc karaktered van, itt esélyt kapsz arra hogy akár 3 karakterrel is játszhatsz egyszerre!</Text>
+            <Text>Ahhoz hogy el tudd kezdeni a játékot először regisztrálnod kell, utána bejelentkezned, majd egy karaktert létrehoznod. Ne aggódj, ha csak egy kedvenc karaktered van, itt esélyt kapsz arra hogy akár 3 karakterrel is játszhatsz egyszerre.!</Text>
             <Text>Hozd létre a saját csapatodat akár 20 fővel, alapíts testvériséget a barátaiddal vagy más játékosokkal, szerezd meg az összes bijuut és legyél TE a legerősebb!</Text>
             <Section cls="mode-content">
                 <Heading text="3">Figyelmeztetés!</Heading>
