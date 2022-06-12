6a0a7c255b7750017b2c3480f6a2c38f881ccb04
kunandras-99 <kunandras99@gmail.com>
2022-06-12 16:26:19 +0200

    start

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -5,17 +5,19 @@ import CharacterImage from '../bottom/CharacterImage';
 import MobileMenuContainer from '../bottom/MobileMenuContainer';
 import ProfileButtons from '../bottom/ProfileButtons';
 import ModeToggle from '../bottom/ModeToggle';
+import Footer from '../site/Footer';
 
 function Layout(props) {
-    const { num } = useState(false);
+    let num = true;
     return (
         <>
             {/* <Header /> */}
             { props.children }
+            <Outlet />
             <MobileMenuContainer />
             { num ? <AuthButtons /> : <><CharacterImage /><ProfileButtons /></> }
             <ModeToggle />
-            <Outlet />
+            <Footer />
         </>
     );
 }
