e2ed58403b4e38692920eb5073a1f1a3491bf714
kunandras-99 <kunandras99@gmail.com>
2022-06-17 01:43:13 +0200

    dev

--- src/components/bottom/mobil-menu/MenuItem.js
+++ src/components/bottom/mobil-menu/MenuItem.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../../../constans/constans";
 function MenuItem(props) {
     return (
         <li className="mobile-menu-item">
-            <span role="button"><img className="mobile-menu-icon" src={ BASE_URL + `assets/images/icons/${ props.icon }-icon.png`} alt={ props.alt } title={ props.title } /></span>
+            <span role="button"><img className="mobile-menu-icon" src={ BASE_URL + `/assets/images/icons/${ props.icon }-icon.png`} alt={ props.alt } title={ props.title } /></span>
             { props.children }
         </li>
     );
